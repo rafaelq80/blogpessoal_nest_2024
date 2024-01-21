811f7fbec825e32d432d1529050b81c60e815608
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-21 14:58:51 -0300

    Update Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { Postagem } from './postagem/entities/postagem.entity';
+import { PostgresService } from './data/services/postgres.service';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'db_blogpessoal',
-    //   entities: [Postagem, Tema, Usuario],
-    //   synchronize: true,
-    // }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      logging: false,
-      dropSchema: false,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      synchronize: true,
-      autoLoadEntities: true,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresService,
+      inject: [PostgresService]
     }),
     PostagemModule,
     TemaModule,
---
+++ src/data/services/postgres.service.ts
@@ -0,0 +1,23 @@
+﻿import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class PostgresService implements TypeOrmOptionsFactory {
+
+  constructor(private configService: ConfigService) { }
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
+}
