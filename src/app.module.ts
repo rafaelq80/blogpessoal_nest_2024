e2f1856f457c4bcc020a258f41c70366a736586f
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-02-27 02:07:46 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { ProdService } from './data/services/prod.service';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { ProdService } from './data/services/Prod.service';
 
 @Module({
   imports: [
